896ad1d76687e599f04d1ec00a128e1fc400782e
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-07-07 15:56:47 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Violão'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Guitarra'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members,
+            newMember:{}
+        }
+    },
+    methods: {
+        addMember(){
+            this.members.push(this.newMember);
+        }
+    }
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
